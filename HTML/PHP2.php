c370f8f7f11d7c2fcce77c4aa5ab4b51e414facd
boula1997 <boula.nessim.soliman@gmail.com>
2023-11-26 20:00:43 +0200

    adding try and catch to connection file

--- HTML/PHP2.php
+++ HTML/PHP2.php
@@ -1,15 +1,22 @@
 <?php
 require_once "pdo.php";
-			
-			$Email=$_POST["Email"];
-			$Password2=$_POST["My_Password"];
 
+try{
+
+	$Email=$_POST["Email"];
+	$Password2=$_POST["My_Password"];
+
+	
+$stmt = $pdo->query("SELECT * FROM creataccount WHERE Email='$Email' AND My_Password='$Password2'");
+if($row = $stmt->fetch(PDO::FETCH_ASSOC))
+header("location:http://localhost/Orange/Home.php?coustmer_id=".$row['id']);
+else
+die("your password don't match with your Email"); 
+
+ } catch (Exception $e) {
+	echo "PDOException: " . $e->getMessage();
+ }
 			
- 	$stmt = $pdo->query("SELECT * FROM creataccount WHERE Email='$Email' AND My_Password='$Password2'");
-		if($row = $stmt->fetch(PDO::FETCH_ASSOC))
-		header("location:http://localhost/Orange/Home.php?coustmer_id=".$row['id']);
-		else
-		die("your password don't match with your Email"); 
 
 		
 
