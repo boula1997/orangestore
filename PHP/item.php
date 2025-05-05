3585f45fccc4e8134e6c6cd08c3884d0311a4f8f
Boula <boula.nessim.soliman@gmail.com>
2025-05-05 20:36:40 +0300

    commit

--- PHP/item.php
+++ PHP/item.php
@@ -1,6 +1,6 @@
 <?php
 echo "<pre>\n";
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=workspace', 'root', '');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=yousabte_workspace', 'yousabte_workspace', 'kD[asKgc%ydC');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // Insert logic
