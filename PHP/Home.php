3123d9a5b8ae5c480956f21daf2e7b1e61ea4ed4
Boula <boula.nessim.soliman@gmail.com>
2025-05-06 20:56:11 +0300

    commit

--- PHP/Home.php
+++ PHP/Home.php
@@ -10,7 +10,7 @@ session_start();
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Laptops Market</title>
-	<link rel="icon" href="../images/icon2.jpg" />
+	<link rel="icon" href="../images/logo.png" />
 	<link rel='stylesheet' href='../css/bootstrap.css' />
 	<link rel='stylesheet' href='../css/font-awesome.min.css' />
 	<link rel='stylesheet' href='../css/style.css' />
