91ac6cdee3973c3602d855a9c649efc161481acc
Boula <boula.nessim.soliman@gmail.com>
2025-05-06 21:44:21 +0300

    commit

--- PHP/Home.php
+++ PHP/Home.php
@@ -319,6 +319,11 @@ session_start();
 	<!-- End Footer -->
 
 
+	<a href="https://wa.me/201234567890" target="_blank" class="whatsapp-float">
+  <img src="https://img.icons8.com/color/48/000000/whatsapp--v1.png" alt="WhatsApp" />
+</a>
+
+
 
 
 	<!--  -->
