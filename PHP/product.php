16c7b1cb2fa7b04f00a6669bd7cf97ed59a736db
Boula <boula.nessim.soliman@gmail.com>
2025-05-06 21:00:48 +0300

    commit

--- PHP/product.php
+++ PHP/product.php
@@ -34,7 +34,7 @@ if (!$product) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Laptop Market</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="../images/icon2.jpg" />
+    <link rel="icon" href="../images/logo.png" />
     <link rel='stylesheet' href='../css/bootstrap.css' />
     <link rel='stylesheet' href='../css/font-awesome.min.css' />
     <link rel='stylesheet' href='../css/style.css' />
