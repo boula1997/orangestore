9d8a41f42cfb4784e352c082d55fd27895b31bbc
Boula <boula.nessim.soliman@gmail.com>
2025-05-06 13:27:21 +0300

    commit

--- PHP/product.php
+++ PHP/product.php
@@ -86,14 +86,34 @@ if (!$product) {
             <table class="table table-striped table-bordered">
                 <thead class="bg-primary text-white">
                     <tr>
-                        <th>السعر</th>
-                        <th>السعر بعد الخصم</th>
+                        <th>Feature</th>
+                        <th>Detail</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td><?= htmlspecialchars($product['price']) ?> ج.م</td>
-                        <td><?= htmlspecialchars($product['price_bd']) ?> ج.م</td>
+                        <td>Processor</td>
+                        <td>Intel Core i7-12700H</td>
+                    </tr>
+                    <tr>
+                        <td>RAM</td>
+                        <td>16 GB DDR5</td>
+                    </tr>
+                    <tr>
+                        <td>Storage</td>
+                        <td>1 TB SSD</td>
+                    </tr>
+                    <tr>
+                        <td>Display</td>
+                        <td>15.6" 4K UHD Touchscreen</td>
+                    </tr>
+                    <tr>
+                        <td>Graphics</td>
+                        <td>NVIDIA RTX 3050 Ti</td>
+                    </tr>
+                    <tr>
+                        <td>Battery Life</td>
+                        <td>Up to 12 hours</td>
                     </tr>
                 </tbody>
             </table>
