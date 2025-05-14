82ddb1a349d96248f106c005da4262f6125c200c
Boula <boula.nessim.soliman@gmail.com>
2025-05-14 17:59:01 +0300

    commit

--- PHP/Home.php
+++ PHP/Home.php
@@ -204,7 +204,7 @@ session_start();
 									alt="img">
 							</div>
 							<ul class="info list-unstyled">
-								<li><?php $row['title'] ?></li>
+								<li><?php echo $row['pt.title']; ?></li>
 								<li>EGP <?php echo number_format($row['price'], 2); ?></li>
 								<li class="right-text">EGP <?php echo number_format($row['price_bd'], 2); ?></li>
 								<br>
